824b624907439b4d6778314ce6a60343eb8635e4
lucaswdm <lucas@data2.com.br>
2019-08-14 07:25:37 -0300

    Create checkServerServices.sh

---
+++ checkServerServices.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+/usr/bin/php /root/data2/checkService.php named
+/usr/bin/php /root/data2/checkService.php php-fpm
+/usr/bin/php /root/data2/checkService.php nginx
+/usr/bin/php /root/data2/checkService.php mysql
