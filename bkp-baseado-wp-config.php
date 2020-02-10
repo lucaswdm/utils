ef56a3a3fd67381d05cb3511a27893b5680d664c
lucaswdm <lucas@data2.com.br>
2020-02-10 18:23:58 -0300

    Update bkp-baseado-wp-config.php

--- bkp-baseado-wp-config.php
+++ bkp-baseado-wp-config.php
@@ -6,3 +6,13 @@ $FILE = $argv[1];
 echo PHP_EOL;
 echo $FILE;
 echo PHP_EOL;
+
+$DIRWP = dirname($FILE) . '/';
+
+if(is_dir($DIRWP))
+{
+     if( is_dir($DIRWP . 'wp-includes/') && is_dir($DIRWP . 'wp-admin/') && is_dir($DIRWP . 'wp-content/') )
+     {
+        $SHELL = "cd ".$DIRWP."; wp-cli db export --allow-root --skip-themes --skip-plugins --path=" . $DIRWP . " " . $DIRWP . ";" . PHP_EOL;
+     }
+}
