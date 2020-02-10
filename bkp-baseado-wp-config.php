53c34e286dd1a2a67f3c0c7804bdde62a5b53725
lucaswdm <lucas@data2.com.br>
2020-02-10 18:32:29 -0300

    Update bkp-baseado-wp-config.php

--- bkp-baseado-wp-config.php
+++ bkp-baseado-wp-config.php
@@ -5,29 +5,29 @@ ini_set('display_errors', true);
 
 $FILE = $argv[1];
 
-echo PHP_EOL;
-echo $FILE;
-echo PHP_EOL;
+#echo PHP_EOL;
+#echo $FILE;
+#echo PHP_EOL;
 
 $DIRWP = dirname($FILE) . '/';
 
 if(!is_dir($DIRWP . 'wp-includes/')&&is_dir($DIRWP . 'htdocs/wp-includes/')) $DIRWP .= 'htdocs/';
-echo $DIRWP . PHP_EOL;
+#echo $DIRWP . PHP_EOL;
 if(!is_dir($DIRWP . 'wp-includes/')&&is_dir($DIRWP . 'www/wp-includes/')) $DIRWP .= 'www/';
-echo $DIRWP . PHP_EOL;
+#echo $DIRWP . PHP_EOL;
 if(!is_dir($DIRWP . 'wp-includes/')&&is_dir($DIRWP . 'public_html/wp-includes/')) $DIRWP .= 'public_html/';
 
-echo $DIRWP . PHP_EOL;
+#echo $DIRWP . PHP_EOL;
 
 if(is_dir($DIRWP))
 {
      echo __FILE__ . "\t" . __LINE__ . "\t" . rand();
      if( is_dir($DIRWP . 'wp-includes/') && is_dir($DIRWP . 'wp-admin/') && is_dir($DIRWP . 'wp-content/') )
      {
-          echo __FILE__ . "\t" . __LINE__ . "\t" . rand();
+          #echo __FILE__ . "\t" . __LINE__ . "\t" . rand();
           $SHELL = "cd ".$DIRWP."; ";
           $SHELL .= "wp-cli db export --allow-root --skip-themes --skip-plugins --path=" . $DIRWP;
-          echo $SHELL . ';' . PHP_EOL;
+          echo PHP_EOL . $SHELL . ';' . PHP_EOL;
      }
      else
      {
