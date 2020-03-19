12ba4e87d01e7a2627661f042a41d1afef7a9e3c
lucaswdm <lucas@data2.com.br>
2020-03-18 21:15:27 -0300

    Update DOMAINS.php

--- DOMAINS.php
+++ DOMAINS.php
@@ -1,5 +1,6 @@
 function validaDominio($domain_name)
 {
+    if(strpos($domain_name, '.') === false) return false;
     return (preg_match("/^([a-z\d](-*[a-z\d])*)(\.([a-z\d](-*[a-z\d])*))*$/i", $domain_name) //valid chars check
             && preg_match("/^.{1,253}$/", $domain_name) //overall length check
             && preg_match("/^[^\.]{1,63}(\.[^\.]{1,63})*$/", $domain_name)   ); //length of each label
