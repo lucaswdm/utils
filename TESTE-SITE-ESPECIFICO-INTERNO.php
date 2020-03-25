125834f2f8fba52a9e2b4cf69cac58f49e5cd2d2
lucaswdm <lucas@data2.com.br>
2020-03-25 00:58:55 -0300

    Update TESTE-SITE-ESPECIFICO-INTERNO.php

--- TESTE-SITE-ESPECIFICO-INTERNO.php
+++ TESTE-SITE-ESPECIFICO-INTERNO.php
@@ -2,7 +2,10 @@
   
   function validacao()
   {
-  
+      $url = 'https://comidinhasdochef.com/';
+      $dados = get_data($url);
+      print_r($dados);
+      return true;
   }
   
   
@@ -14,6 +17,41 @@
       echo $erro;
       exit;
   }
+
+function get_data($url, $POST = false, $FLG_FOLLOW_REDIRECT = false)
+{
+	$ch = curl_init();
+	$timeout = 10;
+	$header = array();
+
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+
+	curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36');
+
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+
+	if($FLG_FOLLOW_REDIRECT)
+	{
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	}
+  
+  if($POST && is_array($POST))
+  {
+      curl_setopt($ch,CURLOPT_POST, true);
+      curl_setopt($ch,CURLOPT_POSTFIELDS, http_build_query($POST));
+  }
+
+	#curl_setopt($ch, CURLOPT_COOKIEJAR, APP_PATH . 'cookie.cookie');
+    #curl_setopt($ch, CURLOPT_COOKIEFILE, APP_PATH . 'cookie.cookie');
+
+	$data = curl_exec($ch);
+  $info = curl_getinfo($ch);
+	curl_close($ch);
+
+	return array($info, $data);
+}
 
 ?>
 <!DOCTYPE html>
