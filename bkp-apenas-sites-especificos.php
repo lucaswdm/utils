50f057a33257f37fc51e787bc3305df37b92a8e0
lucaswdm <lucas@data2.com.br>
2021-03-06 11:15:25 -0300

    Update bkp-apenas-sites-especificos.php

--- bkp-apenas-sites-especificos.php
+++ bkp-apenas-sites-especificos.php
@@ -2,14 +2,10 @@
 
 $DIRBASE = '/var/www/';
 
-$DOMINIOSTXT = mb_strtolower("cartaodecreditoonline.org
-beautyblog.com.br
-creditoparatodos.org
-ideabrasil.com.br
-simpatiaspoderosas.org
-101receitasdegeleia.com
-jeitofitness.com
-blogdakamaleoah.com");
+$DOMINIOSTXT = mb_strtolower("
+xxx.xxx
+xxx.xxx
+");
 
 $DOMINIOS = array_filter(array_map('trim', explode(PHP_EOL, $DOMINIOSTXT)));
 
@@ -17,6 +13,6 @@ foreach($DOMINIOS as $dominio)
 {
   if(!empty($dominio) && is_dir($DIRBASE . $dominio . '/'))
   {
-    echo PHP_EOL . "rsync -avz  /var/www/".$dominio." -e 'ssh -p 22022' root@104.225.219.152:/data/" . $dominio . "/;" . PHP_EOL;
+    echo PHP_EOL . "rsync -avz  /var/www/".$dominio." -e 'ssh -p 22022' root@xxxxx.x.x.x.x.:/data/" . $dominio . "/;" . PHP_EOL;
   }
 }
