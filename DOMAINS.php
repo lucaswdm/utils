08cdf036796fa2db698eebde6d2a80da317b9c25
lucaswdm <lucas@data2.com.br>
2021-10-15 08:15:00 -0300

    Update DOMAINS.php

--- DOMAINS.php
+++ DOMAINS.php
@@ -1,5 +1,6 @@
 function validaDominio($domain_name)
 {
+    if(filter_var($domain_name, FILTER_VALIDATE_IP)) return false;
     if(strpos($domain_name, '.') === false) return false;
     return (preg_match("/^([a-z\d](-*[a-z\d])*)(\.([a-z\d](-*[a-z\d])*))*$/i", $domain_name) //valid chars check
             && preg_match("/^.{1,253}$/", $domain_name) //overall length check
